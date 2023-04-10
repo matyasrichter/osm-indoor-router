6ad1c1e6f8672b00a4a57ca40064b61d45a3154d
Matyáš Richter <richtm12@fit.cvut.cz>
2023-04-10 10:40:50 +0200

    Use a custom method for haversine distance calculation

--- src/GraphBuilding/GeometryExtensions.cs
+++ src/GraphBuilding/GeometryExtensions.cs
@@ -1,17 +1,34 @@
 namespace GraphBuilding;
 
 using System.Diagnostics;
-using CoordinateSharp;
 using NetTopologySuite.Geometries;
 
 public static class GeometryExtensions
 {
-    public static double GetMetricDistance(this Point a, Point b) =>
-        new Distance(a.ToCoordinateSharp(), b.ToCoordinateSharp()).Meters;
-
-    private static CoordinateSharp.Coordinate ToCoordinateSharp(this Point point)
+    /// <summary>
+    /// Get distance in meters using the Haversine formula for great circle distance.
+    /// </summary>
+    public static double GetMetricDistance(this Point a, Point b)
     {
-        Debug.Assert(point.SRID == 4326);
-        return new(point.Y, point.X);
+        Debug.Assert(a.SRID == 4326 && b.SRID == 4326, "SRID must be 4326");
+        const double radius = 6371e3;
+
+        var deltaLat = ToRadians(b.Y - a.Y);
+        var deltaLon = ToRadians(b.X - a.X);
+
+        var x =
+            (Math.Sin(deltaLat / 2) * Math.Sin(deltaLat / 2))
+            + (
+                Math.Cos(ToRadians(a.Y))
+                * Math.Cos(ToRadians(b.Y))
+                * Math.Sin(deltaLon / 2)
+                * Math.Sin(deltaLon / 2)
+            );
+
+        var greatCircleDistance = 2 * Math.Asin(Math.Min(1, Math.Sqrt(x)));
+
+        return radius * greatCircleDistance;
     }
+
+    private static double ToRadians(double angle) => angle * (Math.PI / 180.0);
 }
