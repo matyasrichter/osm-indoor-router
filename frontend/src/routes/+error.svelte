eb95f1b43f38afe71b41d8ab12cd79aaa59cf6c9
Matyáš Richter <richtm12@fit.cvut.cz>
2023-05-09 16:25:46 +0200

    Make the generic error page look better

--- frontend/src/routes/+error.svelte
+++ frontend/src/routes/+error.svelte
@@ -1,1 +1,29 @@
-<div class="centered error">Something went wrong.</div>
+<div class="wrapper">
+	<h1>Something went wrong.</h1>
+    <div>
+        The application is currently experiencing issues.<br>
+        Please try again in a few moments.
+    </div>
+</div>
+
+<style lang="scss">
+    .wrapper{
+        font-family: 'Helvetica Neue', Arial, Helvetica, sans-serif;
+        background-color: #111;
+        color: #f1f1f1;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        font-size: 1.5em;
+
+        h1 {
+            font-size: 3em;
+        }
+        div {
+            text-align: center;
+            line-height: 1.5em;
+        }
+    }
+</style>
